f0ecb78eff8da631f51bebc57a8090eac5553d93
skstlenovo <116804212+skstlenovo@users.noreply.github.com>
2023-05-03 13:05:48 +0530

    2nd attempt

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,2 +1,9 @@
-package com.driver;public class B {
+package com.driver;
+
+public class B extends A{
+    @Override
+    public static String meth()
+    {
+        return "Method is overridden in Extendend class B"
+    }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+      B b=new B();
+      b.meth();
+
+      B b1=new B();
+      b1.meth();
+
+
+  }
 }
